59aedb7288a6670abffd1ca01962abe6853faccd
Brian Balser <brian.balser@gmail.com>
2020-05-04 12:55:54 -0400

    Orc.Type Protocol

---
+++ lib/orc/type.ex
@@ -0,0 +1,10 @@
+defprotocol Orc.Type do
+  @spec to_list(t) :: [Orc.Proto.Type.t()]
+  def to_list(t)
+
+  @spec to_list(t, non_neg_integer()) :: [Orc.Proto.Type.t()]
+  def to_list(t, column)
+
+  @spec column_encoding(t) :: [Orc.Proto.ColumnEncoding.t()]
+  def column_encoding(t)
+end
